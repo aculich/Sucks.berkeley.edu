b823280e46f1fed870e223cee2f658bd5044d7dc
Luke Lu <luke_lmn@berkeley.edu>
2012-10-21 21:43:05 -0700

    changed the params in what_sucks_controller to what-text

--- app/controllers/what_sucks_controller.rb
+++ app/controllers/what_sucks_controller.rb
@@ -3,13 +3,13 @@ class WhatSucksController < ApplicationController
   end
 
   def create
-    issue_name = params[:put][:issue_name]
+    issue_name = params["what-text"]
     if issue_name == "" or issue_name == nil
       flash[:notice] = "Please enter 1-2 words about what sucks!"
       redirect_to "/what_sucks/index" and return
     else
       issue = Issue.new
-      issue.name = params[:put][:issue_name]
+      issue.name = params["what-text"]
       issue.save
       redirect_to "/action/#{issue.id}" and return
     end
