afc42a01d768a18b0e5659d632164df1301ca1fd
Luke Lu <luke_lmn@berkeley.edu>
2012-10-21 22:23:23 -0700

    took out long block comments in feature file

--- features/step_definitions/add_issue_steps.rb
+++ features/step_definitions/add_issue_steps.rb
@@ -56,33 +56,10 @@ Given /^I am on Page 3 for "(.*?)" with the correct information displayed$/ do |
   step "I should see \"What were you doing when the problem occurred?\""
 end
 
-# Make sure that one string (regexp) occurs before or after another one
-#   on the same page
-
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.content  is the entire content of the page as a string.
-  assert /.*#{e1}.*#{e2}/m =~ page.body
-end
-
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
-
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  ratings = rating_list.split(/\,/)
-  ratings.each do|r|
-	if uncheck
-		uncheck("ratings_"+r.gsub(/\s+/,""))
-	else
-		check("ratings_"+r.gsub(/\s+/,""))
-	end
+Then /^(?:|I )should see "([^"]*)"$/ do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
   end
-end
-
-Then /I should see all of the movies/ do
-  assert page.all("table#movies tbody tr").count == Movie.count
 end
