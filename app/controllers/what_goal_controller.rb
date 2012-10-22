53d6e86480898c349e1b01baf86cb6a23ba4870d
kzmeyao <kzmeyao@gmail.com>
2012-10-21 21:59:13 -0700

    changed stuff for cuke

--- app/controllers/what_goal_controller.rb
+++ app/controllers/what_goal_controller.rb
@@ -5,15 +5,15 @@ class WhatGoalController < ApplicationController
     issue = Issue.find_by_id(params[:issue_id])
     @issue_name = issue.name
     @issue_action = issue.action
-    if params[:put]
+    if request.post?
       issue_id = params[:issue_id]
-      issue_goal= params[:put][:issue_goal]
+      issue_goal= params["what-text"]
       if issue_goal == "" or issue_goal == nil
         flash[:notice] = "Please answer the question!"
         redirect_to "/goal/#{issue_id}" and return
       else
         issue = Issue.find_by_id(params[:issue_id])
-        issue.goal = params[:put][:issue_goal]
+        issue.goal = params["what-text"]
         issue.save
         redirect_to "/identity/#{issue_id}" and return
       end
