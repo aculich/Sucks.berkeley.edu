0fd46291148cd7b3762bb15650c5c3abda4d9c31
Tiffany <t.funnee@gmail.com>
2012-10-21 20:30:13 -0700

    completed goal controller rspec test. all else should be modelled after this one

--- spec/controllers/what_goal_controller_spec.rb
+++ spec/controllers/what_goal_controller_spec.rb
@@ -1,18 +1,25 @@
 require "spec_helper"
 
 describe WhatGoalController do
-  describe "Typing in the text box" do
-    it "should look up values for field autocomplete and create JS Array" do
-    end
-  end
+#  describe "Typing in the text box" do
+#    it "should look up values for field autocomplete and create JS Array" do
+#    end
+#  end
   
   describe "Clicking on submit with parameters filled out" do
     it "should link to page_four with name, action, goal parameter" do
+      issue = Issue.create()
+      post :show, :issue_id => issue.id, :put => {"issue_goal" => "finished 169 hw"}, :method => "put"
+      response.should redirect_to("/identity/#{issue.id}")
     end
   end
 
   describe "Clicking on submit with no goal parameter entered" do
     it "should redirect to itself with name, action parameter and flash error message" do
+      issue = Issue.create()
+      new_id = issue.id + 1
+      post :show, :issue_id => issue.id, :method => "put", :put => {"issue_goal" => ""}
+      response.should redirect_to("/goal/#{issue.id}")
     end
   end
 end
--- spec/controllers/what_sucks_controller_spec.rb
+++ spec/controllers/what_sucks_controller_spec.rb
@@ -11,17 +11,13 @@ describe WhatSucksController do
       issue = Issue.create()
       new_id = issue.id + 1
       post :create, :put => {"issue_name" => "asdf"}, :method => "put"
-#      response.body.should include "Sucks!"
-#      response.body.should include issue.name
       response.should redirect_to("/action/#{new_id}")
     end
   end
 
   describe "clicking on the submit button with no name parameter entered" do
     it "should redirect to itself and flash and error message" do
-      issue = Issue.create()
       post :create, :method => "put", :put => {"issue_name" => ""}
-#     response.body.should include "Sucks!"
       response.should redirect_to("/what_sucks/index")
     end
   end
