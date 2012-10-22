7db748c8304672bc18ac1cc2bfae9c319468c636
Tiffany <t.funnee@gmail.com>
2012-10-21 20:54:02 -0700

    rspec tests completed-- 100 percent coverage yeeeeeeeaaaaaaaaah

--- spec/controllers/identity_controller_spec.rb
+++ spec/controllers/identity_controller_spec.rb
@@ -1,24 +1,28 @@
 require "spec_helper"
 
 describe IdentityController do
-  describe "typing in the text box" do
-    it "should look up values for field autocomplete and create JS Array" do
-    end
+#  describe "typing in the text box" do
+#    it "should look up values for field autocomplete and create JS Array" do
+#    end
 
-    it "should look up values from" do
-    end
-  end
+#    it "should look up values from" do
+#    end
+#  end
 
-  describe "clicking on the submit button" do
-    it "should save an entry to the database" do
-    end
-
+  describe "clicking on the submit button with parameters" do
     it "should link to page_five with name, action, goal, identity parameter" do
+      issue = Issue.create()
+      post :show, :issue_id => issue.id, :put => {"identity" => "cookie monster"}, :method => "put"
+      response.should redirect_to("/summary/#{issue.id}")
     end
   end
 
   describe "clicking on the submit button with no identity parameter" do
     it "should redirect to itself with name, action, goal parameter and flash error message" do
+      issue = Issue.create()
+      new_id = issue.id + 1
+      post :show, :issue_id => issue.id, :method => "put", :put => {"identity" => ""}
+      response.should redirect_to("/identity/#{issue.id}")
     end
   end
 end
