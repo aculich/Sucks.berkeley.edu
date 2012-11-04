499e19811176170888d2832254b5d23ea8a4a8f8
Luke Lu <luke_lmn@berkeley.edu>
2012-11-04 15:37:51 -0800

    finsihed specific_issue_controller.rb

--- app/controllers/specific_issue_controller.rb
+++ app/controllers/specific_issue_controller.rb
@@ -4,28 +4,30 @@ class SpecificIssueController < ApplicationController
   end
 
   def show
+    debugger
     @issue_name = params[:issue_name]
-	
-	if i_want == "" or i_want == nil
-	  flash[:notice] = "Please tell us what you want!"
-	  redirect_to "/specific_issue/#{issue_name}"
-	end
-	
-	if so_that == "" or so_that == nil
-	  flash[:notice] = "Please tell us why you want this feature!"
-	  redirect_to "/specific_issue/#{issue_name}"
-	end
+	i_want = params["want-text"]
+	so_that = params["so-text"]
 	
 	if request.post?
-		Issue.create!({"name"=>params["issue_name"], 
-					   "i_want"=>params["what-text"], 
-					   "so_that"=>params["so-text"], 
-					   "votes"=>0
-					   "created_at"=>Time.now})
+		if i_want == "" or i_want == nil
+			flash[:notice] = "Please tell us what you want!"
+			redirect_to "/specific_issue/#{issue_name}"
+		end
+		
+		if so_that == "" or so_that == nil
+			flash[:notice] = "Please tell us why you want this feature!"
+			redirect_to "/specific_issue/#{issue_name}"
+		end
+	
+		issue_id = Issue.create!({"name"=>params["issue_name"], 
+					   "i_want"=>i_want, 
+					   "so_that"=>so_that, 
+					   "votes"=>0}).id
+		redirect_to "/issue_info/#{issue_id}"
 	
 	else
-		issue_name = params["what-issue"]
-		issues = Issue.find_all_by_name(:issue_name)
+		issues = Issue.find_all_by_name(@issue_name)
 =begin
 these are comments:
 	  issue = Issue.find_by_id(params[:issue_id])
