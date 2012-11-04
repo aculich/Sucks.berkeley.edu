4b4af1eb7d1dc1bf71485edf98670cb63afc2ce9
kzmeyao <kzmeyao@gmail.com>
2012-11-04 13:57:19 -0800

    Issue info page now pulls real data for user stories

--- app/controllers/issue_info_controller.rb
+++ app/controllers/issue_info_controller.rb
@@ -4,6 +4,7 @@ class IssueInfoController < ApplicationController
   end
 
   def show
+    @issue = Issue.find_by_id(params[:issue_id])
     @issue_name = Issue.find_by_id(params[:issue_id]).name
     #@comment = Issue.comment.where(:issue_name => @issue_name)
   end
