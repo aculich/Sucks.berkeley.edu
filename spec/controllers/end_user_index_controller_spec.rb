03dcdec062954e139bb72f6da0a2978eda6ed5ca
Luke Lu <luke_lmn@berkeley.edu>
2012-11-04 17:46:01 -0800

    wrote Rspec for what_sucks and end_user_index

--- spec/controllers/end_user_index_controller_spec.rb
+++ spec/controllers/end_user_index_controller_spec.rb
@@ -2,25 +2,22 @@ require "spec_helper"
 
 describe EndUserIndexController do
 
-  describe "Clicking on the submit button with your issue filled out" do
-    it "should link to specific_issue with issue parameter filled out" do
-      issue = Issue.create()
-      #post :show, :issue_id => issue.id, "what-text" => "bear", :method => "put"
-      response.should redirect_to("/specific_issue/#{issue.name}")
+  describe "following Telebears Sucks submit button" do
+    it "should call the find_all_by_name methods in the model" do
+	  @mock = mock('Issue')
+	  @mock.stub!(:name).and_return("Telebears")
+	  @fake_results = [mock('Issue'), mock('Issue')]
+	  Issue.should_receive(:find_all_by_name).with(@mock.name).and_return(@fake_results)
+	  get :show, :issue_name => "Telebears"
     end
   end
 
+=begin
   describe "Clicking on one of the given Pivotal Tracker or User Voice issues" do
     it "should link to specific_issue with issue parameter filled out" do
       response.should redirect_to("/specific_issue/#{issue.name}")
     end
   end
+=end
 
-  describe "clicking on the submit button with no issue parameter entered" do
-    it "should redirect to itself and flash and error message" do
-      issue = Issue.create()
-      #post :show, :issue_id => issue.id, :method => "put", "what-text" => ""
-      response.should redirect_to("/end_user_index/index")
-    end
-  end
 end
