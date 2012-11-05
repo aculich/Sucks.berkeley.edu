548fe149b800339ffbbceab3e4191b07169f0995
kzmeyao <kzmeyao@gmail.com>
2012-11-04 17:50:33 -0800

    fixed some spec files

--- spec/controllers/issue_info_controller_spec.rb
+++ spec/controllers/issue_info_controller_spec.rb
@@ -1,7 +1,14 @@
 require "spec_helper"
 
 describe IssueInfoController do
-
+  describe "Arriving on issue info page" do
+    it "should contain issue details" do
+      issue = Issue.create()
+      issue.name = "Airbears"
+      get :show, :issue_id => issue.id
+      assigns(:issue).should == issue
+    end
+  end
 
   describe "Clicking on the submit question button" do
     it "should post clarificational question by developer" do
--- spec/controllers/specific_issue_controller_spec.rb
+++ spec/controllers/specific_issue_controller_spec.rb
@@ -2,21 +2,20 @@ require "spec_helper"
 
 describe SpecificIssueController do
 
-
-  
-
   describe "Clicking on the submit button with parameters filled out" do
     it "should link to the issue_info with i_want and so_that parameters" do
       issue = Issue.create()
-      post :show, :issue_id => issue.id, "want-text" => "I want candy", "so-text" => "cavities", :method => "put"
-      response.should redirect_to("/issue_info/#{issue.name}")
+      issue.name = "Airbears"
+      post :show, :issue_name => issue.name, "want-text" => "I want candy", "so-text" => "cavities", :method => "put"
+      response.should redirect_to("/issue_info/#{issue.id + 1}")
     end
   end
 
   describe "clicking on the submit button with no i_want parameter entered" do
     it "should redirect to itself with so_that parameter and flash error message" do
       issue = Issue.create()
-      post :show, :issue_id => issue.id, "want-text" => "", "so-text" => "cavities", :method => "put"
+      issue.name = "Airbears"
+      post :show, :issue_name => issue.name, "want-text" => "", "so-text" => "cavities", :method => "put"
       response.should redirect_to("/specific_issue/#{issue.name}")
     end
   end
@@ -24,7 +23,8 @@ describe SpecificIssueController do
   describe "clicking on submit button with no so_that parameter entered" do
     it "should redirect to itself with i_want parameter and flash error message" do
       issue = Issue.create()
-      post :show, :issue_id => issue.id, "want-text" => "I want candy", "so-text" => "", :method => "put"
+      issue.name = "Airbears"
+      post :show, :issue_name => issue.name, "want-text" => "I want candy", "so-text" => "", :method => "put"
     end
   end
 end
