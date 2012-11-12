3f19199e8ecae39c8d31ca2d769c8124c391e7be
Tiffany <t.funnee@gmail.com>
2012-11-12 10:53:30 -0800

    added rspec stories for developer index and request form page

---
+++ spec/controllers/developer_issue_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe DeveloperIndexController do
+
+  describe "Retrieving pivotal tracker and user voice data" do
+    describe "should call model method to retrieve similar pivotal tracker stories" do
+    end
+
+    describe "should call model method to retrieve similar uservoice stories" do
+    end
+  end
+
+end
---
+++ spec/controllers/request_form_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+describe RequestFormController do
+  describe "Clicking on the submit information button" do
+    it "should create a user profile for in pivotal tracker" do
+    end
+
+    it "should redirect to issue_info page" do
+    end
+  end
+
+end
