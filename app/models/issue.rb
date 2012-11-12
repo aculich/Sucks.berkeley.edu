eee1ad886933314d885b752a20079d7ebf8b0474
Tiffany <t.funnee@gmail.com>
2012-11-12 12:48:54 -0800

    added project model and edited issue model

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,7 @@
 class Issue < ActiveRecord::Base
   # attr_accessible :action, :date_entered, :goal, :name, :identity_tags
   has_many :comments
+  belongs_to :Project
   attr_accessible :name, :link_to_uservoice, :date_entered, :votes, :action, :as_a, :so_that, :i_want
   # has_many :identity_tags
   # TODO make has_many with identity_tags, but we actually have to make the class
