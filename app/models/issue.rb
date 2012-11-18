9cbe5a5c265d56d5c20376570fdf287c3004e4dc
Luke Lu <luke_lmn@berkeley.edu>
2012-11-18 13:33:01 -0800

    took out debugger staement:

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,12 +1,11 @@
 require 'pivotal-tracker'
+require 'uservoice-ruby'
 
 class Issue < ActiveRecord::Base
   # attr_accessible :action, :date_entered, :goal, :name, :identity_tags
   has_many :comments
   belongs_to :Project
   attr_accessible :name, :link_to_uservoice, :date_entered, :votes, :action, :as_a, :so_that, :i_want
-  debugger
-  x = 9
   # has_many :identity_tags
   # TODO make has_many with identity_tags, but we actually have to make the class
 end
