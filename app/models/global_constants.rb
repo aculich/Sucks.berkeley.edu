90dab6db9db164cf456783d2d4e37a64b8d5bfa0
Luke Lu <luke_lmn@berkeley.edu>
2012-11-18 21:16:56 -0800

    added global_constants to contain the API keys and stuff

---
+++ app/models/global_constants.rb
@@ -0,0 +1,7 @@
+module GlobalConstants
+  CLIENT_TOKEN = "e78db5b6127c5d5529d236c464f295b3"
+  UV_SUBDOMAIN = "google5"
+  UV_API_KEY = "NsOgv6KCGyHQaOhSghlow"
+  UV_API_SECRET = "88OahJ68hkjHpcIYncmqgEAW88RszQMjT4zQmtng"
+  CALLBACK_URL = "http://localhost:3000/"
+end
