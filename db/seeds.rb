abfd8a3e34cf0d93cbcda880c1b6665611669743
kzmeyao <kzmeyao@gmail.com>
2012-11-18 23:28:41 -0800

    limiting seed votes to below 3 so no stories are duplicated on pivotal tracker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@ projects_names.each do |name|
   Project.create_pivotal_project(name)
 end
 
-issues = [{:name => 'KevinYao', :action => 'talking to him', :date_entered => '25-Nov-1992', :votes => 3, :as_a => '169 TA', :so_that => 'I can do my job', :i_want => 'to fail him'},
-		  {:name => 'Airbears', :action => 'Watching Hulu', :date_entered => '25-Dec-2001', :votes => 4, :as_a => 'student', :so_that => 'I can watch hulu', :i_want => 'to get internet access'},
+issues = [{:name => 'KevinYao', :action => 'talking to him', :date_entered => '25-Nov-1992', :votes => 2, :as_a => '169 TA', :so_that => 'I can do my job', :i_want => 'to fail him'},
+		  {:name => 'Airbears', :action => 'Watching Hulu', :date_entered => '25-Dec-2001', :votes => 1, :as_a => 'student', :so_that => 'I can watch hulu', :i_want => 'to get internet access'},
 		  {:name => 'TiffanyJianto', :action => 'breaking her chair', :date_entered => '3-Nov-2012', :votes => 1, :as_a => '169 TA', :so_that => 'I can do my job', :i_want => 'to fail her'},
 		  {:name => 'KevinYao', :action => 'buying bagels', :date_entered => '2-Apr-1992', :votes => 2, :as_a => '169 TA', :so_that => 'I can do my job', :i_want => 'to pass her'},
-		  {:name => 'TiffanyJianto', :action => 'sitting', :date_entered => '25-Nov-2045', :votes => 5, :as_a => '169 TA', :so_that => 'I can do my job', :i_want => 'to pass him'},
+		  {:name => 'TiffanyJianto', :action => 'sitting', :date_entered => '25-Nov-2045', :votes => 2, :as_a => '169 TA', :so_that => 'I can do my job', :i_want => 'to pass him'},
 		  {:name => 'Telebears', :action => 'signing up for classes', :date_entered => '1-Feb-2011', :votes => 0, :as_a => 'administrator', :so_that => 'mess with students', :i_want => 'to make telebears suck'},
 		 ]
 
