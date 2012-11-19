06c6f1a844ca43fa52f32e970d085b92ad127b8a
kzmeyao <kzmeyao@gmail.com>
2012-11-18 21:22:18 -0800

    end user index spec updated

--- spec/controllers/end_user_index_controller_spec.rb
+++ spec/controllers/end_user_index_controller_spec.rb
@@ -9,14 +9,18 @@ describe EndUserIndexController do
 	    @fake_results = [mock('Issue'), mock('Issue')]
 	    Issue.should_receive(:find_all_by_name).with(@mock.name).and_return(@fake_results)
 	    get :show, :issue_name => "Telebears"
+        assigns(:issues).should == @fake_results
     end
   end
 
   describe "retrieving pivotal tracker and uservoice data" do
-    it "should call model method to retrieve similar pivotal tracker stories" do
-    end
-
     it "should call model method to retrieve similar uservoice tickets" do
+        @mock = mock('Uservoice_ticket')
+	    @mock.stub!(:subject).and_return("Telebears")
+	    @fake_results = [mock('Uservoice_ticket'), mock('Uservoice_ticket')]
+        Uservoice_ticket.should_receive(:find_all_by_subject).with(@mock.subject).and_return(@fake_results)
+        get :show, :issue_name => "Telebears"
+        assigns(:uservoice_tickets).should == @fake_results
     end
   end
 
