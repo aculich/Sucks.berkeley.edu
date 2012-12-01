fa8b8cee2abc1ba609b69a7e3ffe5ba4ce0dc679
Tiffany <t.funnee@gmail.com>
2012-12-01 12:44:52 -0800

    implemented upvoting abuse so user can upvote issue once, but as many issues as they want. if they try to upvote mult times, i flash notice, so doesn't do anything noticeable

--- app/controllers/issue_info_controller.rb
+++ app/controllers/issue_info_controller.rb
@@ -7,9 +7,14 @@ class IssueInfoController < ApplicationController
     @issue = Issue.find_by_id(params[:issue_id])
     @issue_name = @issue.name
     if params["vote"] == "true"
-      @issue.votes += 1
-      Project.add_issue_to_pivotal(@issue)             
-		  @issue.save!
+      if (not session["upvoted_issues"]) or (session["upvoted_issues"] and session["upvoted_issues"][@issue.id] != "true")
+        @issue.votes += 1
+        Project.add_issue_to_pivotal(@issue)             
+		    @issue.save!
+        session["upvoted_issues"] = {@issue.id => "true"}
+      elsif session["upvoted_issues"] and session["upvoted_issues"][@issue.id] == "true"
+        flash[:notice] = "You've already upvoted this issue!"
+      end
     end
     @comments = @issue.comments
     if @comments != nil
