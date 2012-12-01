5f90946aa5ff29e3ca203e48fdfd3b24de833e31
Luke Lu <luke_lmn@berkeley.edu>
2012-12-01 14:22:03 -0800

    fixed what ashley the noob broke :) aka capitalization errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@ end
 
 issues = [{:name => 'Telebears', :action => 'signing up for classes', :date_entered => '25-Nov-1992', :votes => 2, :as_a => 'fourth year undergraduate student', :so_that => 'I can get into a class I need to graduate in four years', :i_want => 'Telebears not to crash'},
 		  {:name => 'Airbears', :action => 'sitting in lecture', :date_entered => '25-Dec-2001', :votes => 1, :as_a => 'EECS student', :so_that => 'I can download lecture notes', :i_want => 'to get internet access in Soda Hall during lecture'},
-		  {:name => 'Schedule.berkeley.edu', :action => 'creating a schedule for next semester', :date_entered => '3-Nov-2012', :votes => 1, :as_a => 'incoming freshman', :so_that => 'i can more easily create my first-semester schedule ', :i_want => 'to be able to view courses that satisfy my major and that are available'},
-		  {:name => 'Bspace', :action => 'studying for midterm', :date_entered => '2-Apr-1992', :votes => 2, :as_a => 'student in Anthro 160AC', :so_that => 'I can quickly download an entire semesters worth of lecture notes', :i_want => 'to be able to checkmark multiple files to download at one time'},
-		  {:name => 'Piazza', :action => 'submitting iteration 3-2', :date_entered => '25-Nov-2045', :votes => 2, :as_a => 'CS169 student', :so_that => 'my entire group can view and edit our Piazza post', :i_want => 'to be able to share private Piazza posts with multiple students'},
+		  {:name => 'schedule.berkeley.edu', :action => 'creating a schedule for next semester', :date_entered => '3-Nov-2012', :votes => 1, :as_a => 'incoming freshman', :so_that => 'i can more easily create my first-semester schedule ', :i_want => 'to be able to view courses that satisfy my major and that are available'},
+		  {:name => 'bspace', :action => 'studying for midterm', :date_entered => '2-Apr-1992', :votes => 2, :as_a => 'student in Anthro 160AC', :so_that => 'I can quickly download an entire semesters worth of lecture notes', :i_want => 'to be able to checkmark multiple files to download at one time'},
+		  {:name => 'piazza', :action => 'submitting iteration 3-2', :date_entered => '25-Nov-2045', :votes => 2, :as_a => 'CS169 student', :so_that => 'my entire group can view and edit our Piazza post', :i_want => 'to be able to share private Piazza posts with multiple students'},
 		 ]
 
 issues.each do |issue|
