855a0eb8118afae788d617747d66441a69bd100e
kzmeyao <kzmeyao@gmail.com>
2012-12-02 16:15:11 -0800

    dev comments implemented

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :issue
-  attr_accessible :commentor_name, :date, :details, :issue_id
-  attr_accessible :from_developer
+  attr_accessible :commentor_name, :date, :details, :issue_id, :from_developer
 end
